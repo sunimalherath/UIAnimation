c63f98e6bc8d5ac5f220346b15f6593525305af2
cod3breaker <ravisherath@gmail.com>
2020-06-06 18:57:49 +1000

    Initial Commit

---
+++ UIAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIAnimation
+//
+//  Created by Sunimal Herath on 6/6/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
